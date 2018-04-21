7b9281e9664d9586f95ec2e3ae22583f14d5ec40
mmizera <mizeramaciejgit@gmail.com>
2018-04-21 23:35:43 +0200

    more colors; add loop through colors

--- client/reducers/colorMap.js
+++ client/reducers/colorMap.js
@@ -1,3 +1,3 @@
 export default function (state, action) {
-  return { 1: 'red', 2: 'blue', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'purple', 7:'pink' };
+  return { 1: 'red', 2: 'blue', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'purple', 7:'pink', 0:'darkgreen'};
 }
