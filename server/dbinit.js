e80acfb5190370a3ec7236eabdae37b6ed27dd64
mmizera <mizeramaciejgit@gmail.com>
2018-04-21 19:00:31 +0200

    add dbinit with sample categories; usage: 'node dbinit.js'

--- server/dbinit.js
+++ server/dbinit.js
@@ -2,7 +2,83 @@ const mysql = require('mysql');
 const dbConnectionInfo = require('./dbConf.json');
 const pool = mysql.createPool(dbConnectionInfo);
 
-createTables();
+
+let p = new Promise((resolve, reject) => {
+  executeSql("CREATE DATABASE IF NOT EXISTS fastodos;",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+});
+
+p.then(new Promise((resolve) => {
+  executeSql("USE fastodos;",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+}));
+
+p.then(new Promise((resolve) => {
+  executeSql("CREATE TABLE IF NOT EXISTS `categories` (categoryId INT AUTO_INCREMENT PRIMARY KEY, categoryName VARCHAR(255))",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+}));
+
+p.then(new Promise((resolve) => {
+  executeSql("CREATE TABLE IF NOT EXISTS `todos` (todoId INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(255)," +
+    " finished BOOL, categoryId INT, FOREIGN KEY (categoryId) REFERENCES categories(categoryId))",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+}));
+
+
+p.then(new Promise((resolve) => {
+  executeSql("INSERT INTO categories(categoryId,categoryName) " +
+    " SELECT '1','Other' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Other');",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+}));
+
+p.then(new Promise((resolve => {
+  executeSql("INSERT INTO categories(categoryId,categoryName) " +
+    " SELECT '2','Home' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Home');",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+})));
+
+p.then(new Promise((resolve => {
+  executeSql("INSERT INTO categories(categoryId,categoryName) " +
+    " SELECT '3','Work' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Work');",
+    (err, result) => {
+      if (err) {
+        throw err;
+      }
+      resolve();
+    });
+})));
+
+p.then(process.exit);
 
 function executeSql(query, callback) {
   pool.getConnection((err, con) => {
@@ -11,52 +87,4 @@ function executeSql(query, callback) {
     }
     con.query(query, callback);
   });
-}
-
-function createTables() {
-  executeSql("CREATE DATABASE IF NOT EXISTS fastodos;",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("USE fastodos;",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("CREATE TABLE IF NOT EXISTS `categories` (categoryId INT AUTO_INCREMENT PRIMARY KEY, categoryName VARCHAR(255))",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("CREATE TABLE IF NOT EXISTS `todos` (todoId INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(255), finished BOOL, categoryId INT, FOREIGN KEY (categoryId) REFERENCES categories(categoryId))",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("INSERT INTO categories(categoryId,categoryName) " +
-    " SELECT '1','Other' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Other');",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("INSERT INTO categories(categoryId,categoryName) " +
-    " SELECT '2','Home' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Home');",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
-  executeSql("INSERT INTO categories(categoryId,categoryName) " +
-    " SELECT '3','Work' WHERE NOT EXISTS(SELECT * FROM categories WHERE categoryName='Work');",
-    (err, result) => {
-      if (err) {
-        throw err;
-      }
-    });
 }
