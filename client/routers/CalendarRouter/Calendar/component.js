68052af635863ebea100a407d506a920ac94a788
chudy1997 <karolbartyzel@interia.pl>
2018-05-04 12:03:54 +0200

    Style calendar. Add arrows.

--- client/routers/CalendarRouter/Calendar/component.js
+++ client/routers/CalendarRouter/Calendar/component.js
@@ -5,24 +5,33 @@ const moment = require('moment');
 
 export default class Calendar extends Component {
     state = {
-        chosenDate: new Date()
+        startTime: moment({h: 6, m: 0}),
+        endTime: moment({h: 18, m: 1})
     };
 
     onChange = date => this.setState({ chosenDate: date });
 
-    createWeekCalendar = () => <WeekCalendar
+    createWeekCalendar = () => <WeekCalendar 
         dayFormat={'ddd DD.MM'}
-        startTime={ moment({h: 6, m: 0}) }
-        endTime={ moment({h: 18, m: 1}) }
-        scaleUnit={30}
-        scaleHeaderTitle='FasTodos'
-        cellHeight={25}
+        startTime = {this.state.startTime}
+        endTime = {this.state.endTime}
+        scaleUnit = {30}
+        scaleHeaderTitle = 'FasTodos'
+        cellHeight = {25}
     />;
 
     render = () => {
         return (
             <div className='calendar'>
-                {this.props.toggle}
+                <div className="calendar-switch">
+                    {this.props.toggle}
+                    <span class='switch'>
+                        <i class="arrow left"></i>
+                    </span>
+                    <span class='switch'>                    
+                        <i class="arrow right"></i>
+                    </span>
+                </div>
                 <div className='week-calendar'>
                     {this.createWeekCalendar()}
                 </div>
