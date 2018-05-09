47c00ed11f78747b9b2ce38ee9e664cd483c5658
kiccek <kiccek@gmail.com>
2018-05-10 01:05:47 +0200

    fixed coloring todos from deleted category

--- client/components/Categories/container.js
+++ client/components/Categories/container.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 
 import fetchCategories from './../../actions/fetchCategories';
+import fetchTodos from './../../actions/fetchTodos';
 import chooseCategory from './../../actions/chooseCategory';
 
 function mapStateToProps(state){
     return {
         categories: state.categories,
         chosenCategoryId: state.chosenCategoryId,
+        todos: state.todos,
         colorMap: state.colorMap
     };
 }
 
 function matchDispatchToProps(dispatch){
-    return bindActionCreators({fetchCategories: fetchCategories, chooseCategory: chooseCategory}, dispatch);
+    return bindActionCreators({fetchCategories: fetchCategories, fetchTodos: fetchTodos, chooseCategory: chooseCategory}, dispatch);
 }
 
 export default {
     mapStateToProps, 
     matchDispatchToProps
-}; 
+};
