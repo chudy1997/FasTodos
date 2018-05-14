c1215d778807e0ef6b75866c6a9359d80296d398
Adam Dyszy <electro.ubro@gmail.com>
2018-05-14 05:59:49 +0200

    changes colors to be nicer on phone or other screens

--- client/reducers/colorMap.js
+++ client/reducers/colorMap.js
@@ -1,3 +1,3 @@
 export default function (state, action) {
-  return { 1: 'red', 2: 'blue', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'purple', 7:'pink', 0:'darkgreen'};
+  return { 1: 'red', 2: 'aqua', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'magenta', 7:'pink', 0:'darkgreen'};
 }
