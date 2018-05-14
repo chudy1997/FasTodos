5710cce5151751cab034695ff6eba55d45f7939b
Adam Dyszy <electro.ubro@gmail.com>
2018-05-14 06:01:22 +0200

    colors should be more pleasant to eye now

--- client/reducers/colorMap.js
+++ client/reducers/colorMap.js
@@ -1,3 +1,3 @@
 export default function (state, action) {
-  return { 1: 'red', 2: 'aqua', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'magenta', 7:'pink', 0:'darkgreen'};
+  return { 1: 'DarkSalmon', 2: 'aqua', 3: 'green', 4: 'yellow', 5: 'orange', 6: 'MediumTurquoise', 7:'pink', 0:'darkgreen'};
 }
