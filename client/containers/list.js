cd23785efad22a684242cc8ee3b75d7d51b4a6d7
chudy1997 <karolbartyzel@interia.pl>
2018-05-21 23:04:39 +0200

    Expand todo on click.

--- client/containers/list.js
+++ client/containers/list.js
@@ -17,30 +17,16 @@ class List extends Component {
             this.props.fetchTodos(todos.sort((a, b) => b.todoId - a.todoId).sort((a, b) => a.finished < b.finished ? -1 : 1));
         }, 
         () => {
-            alert('Could not fetch todos from db with 5 tries at maximum 6 sec...');
+            alert('Could not fetch todos...');
         });
     };
 
-    handleSubmit = (e) => {
-        const text = this.state.input.trim();
-        if (text.length > 0){
-            const todos = this.props.todos;
-            let categoryId = this.getCategoryId();
-            if (categoryId===0) 
-                categoryId=1;
-
-            ajax('POST', `todos/new?text=${text}&categoryId=${categoryId}`, 5, 1000, res => {
-                const newTodo = { todoId: res.insertId, text: text, categoryId: categoryId };
-                todos.unshift(newTodo);
-                this.props.fetchTodos(todos);
-            }, 
-            () => {
-                alert('Could not add new todo to db with 5 tries at maximum 6 sec...');
-            });
-
-            this.setState({ input: '' });
-        }
-        e.preventDefault();
+    fetchChangedTodos(todos, todo){
+        const index = todos.findIndex(t => t.todoId === todo.todoId);
+        todos[index] = {
+            ...todo
+        };
+        this.props.fetchTodos(todos);
     }
 
     getCategoryId = () => {
@@ -48,174 +34,157 @@ class List extends Component {
         return category ? category.categoryId : 0;
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const text = this.state.input.trim();
+        if (text.length > 0){
+            const todos = this.props.todos;
+            let categoryId = this.getCategoryId();
+            if (categoryId === 0) 
+                categoryId = 1;
+
+            ajax('POST', `todos/new?text=${text}&categoryId=${categoryId}`, 5, 1000, res => {
+                const newTodo = { todoId: res.insertId, text: text, categoryId: categoryId };
+                todos.unshift(newTodo);
+                this.props.fetchTodos(todos);
+            }, 
+            () => {
+                alert('Could not add new todo...');
+            });
+
+            this.setState({ input: '' });
+        }
+    }
+
+    handleChange = (e) => this.setState({ input: e.target.value });
+
+    handleExpand = (todo) => {
+        this.props.chooseTodo(this.props.chosenTodoId !== todo.todoId ? todo.todoId : -1);
+    };
+
+    handleCheck = (e, todo) => {      
+        e.stopPropagation();
+        const todos = this.props.todos;
+        const oldValue = todo.finished;
+        const newValue = oldValue ? 0 : 1;
+
+        todo.finished = newValue;
+        this.fetchChangedTodos(todos, todo);
+        ajax('POST', `todos/finish?id=${todo.todoId}&value=${newValue}`, 5, 1000, () => {}, 
+        () => {
+            alert('Could not check todo...');
+            todo.finished = oldValue;
+            this.fetchChangedTodos(todos, todo);
+        });
+    };
+
+     handleDelete = (e, todo) => {
+        e.stopPropagation();
+        if (confirm("Are you sure that you want to delete this todo?")) {
+            const todos = this.props.todos;
+
+            ajax('POST', `todos/delete?id=${todo.todoId}`, 5, 1000, () => {
+                todos.splice(todos.findIndex(t => t.todoId === todo.todoId), 1);
+                this.props.fetchTodos(todos);
+            }, 
+            () => {
+                alert('Could not delete todo...');
+            });
+        }
+
+        return false;
+    };
+
     createTodos = () => {
-        let id = -1;
         const categoryId = this.getCategoryId();
         const todos = this.props.todos.filter(todo => categoryId === 0 || todo.categoryId === categoryId);
 
-    return todos.map(todo => {
-      id += 1;
-      const backgroundColor =  this.props.colorMap[todo.categoryId%Object.keys(this.props.colorMap).length];
-      const className = this.props.chosenTodoId == id ? 'todo chosen-todo' : 'todo';
+        return todos.map(todo => {
+            const backgroundColor =  this.props.colorMap[todo.categoryId%Object.keys(this.props.colorMap).length];
+            const className = this.props.chosenTodoId == todo.todoId ? 'todo chosen-todo' : 'todo';
 
-        return <li
-            className={className}
-            id={id}
-            key={id}
-            onClick={this.chooseTodo}
-            style={{backgroundColor: backgroundColor}}
-        >
-            {todo.text}
-            <button
-                onClick={(e) => this.handleDelete(e, todo.todoId)}
-                className="buttonstyle"
+            return <li
+                className={className}
+                id={todo.todoId}
+                key={todo.todoId}
+                onClick={() => this.handleExpand(todo)}
+                style={{backgroundColor: backgroundColor}}
             >
-                {'✘'}
-            </button>
-            <button
-                onClick={(e) => this.handleExpand(e, todo.todoId)}
-                className="buttonstyle"
-            >
-                {'▼'}
-            </button>
-            <button
-                onClick={(e) => this.handleCheck(e, todo.todoId)}
-                className="buttonstyle"
-            >
-                {todo.finished ? '✔' : '-'}
-            </button>
-            <div className="panel">
-                <p className={todo.clicked == 1 ? 'view' : 'noview'}>
-                    <form
-                        className="form"
-
-                    >
-                        {this.props.toggle}
-                        <input
-                            autoFocus="autofocus"
-                            className="input"
-                            maxLength="50"
-                            onChange={this.handleChange}
-                            placeholder="date"
-                            type="text"
-                            value={this.state.input}
-                        />
-                        <input
-                            autoFocus="autofocus"
-                            className="input"
-                            maxLength="50"
-                            onChange={this.handleChange}
-                            placeholder="categorie"
-                            type="text"
-                            value={this.state.input}
-                        />
-                    </form>
-                </p>
-            </div>
-
-
-        </li>;
-    });
-  }
-
-    handleChange = (e) => this.setState({ input: e.target.value });
-    chooseTodo = (e) => this.props.chooseTodo(e.target.id);
-
-    handleExpand= (e, todoId) => {
-
-        const todos = this.props.todos;
-        const todo = todos.find(todo => todo.todoId === todoId);
-        if (todo.clicked !== 1) {
-            todo.clicked = 1;
-        } else {
-            todo.clicked = 0;
-        }
-
-        const index = todos.findIndex(t => t.todoId === todoId);
-        let newTodo={};
-        Object.assign(newTodo,todo);
-        todos[index] = newTodo;
-        this.props.fetchTodos(todos);
-
-    };
-
-    handleCheck = (e, todoId) => {
-        const todos = this.props.todos;
-        const todo = todos.find(todo => todo.todoId === todoId);
-        const newFinishedValue = todo.finished ? 0 : 1;
-        todo.finished = newFinishedValue;
-        ajax('POST', `todos/finish?id=${todo.todoId}&value=${newFinishedValue}`, 5, 1000, () => {}, 
-        () => {
-            alert('Could not check todo in db with 5 tries at maximum 6 sec...');
+                {todo.text}
+                <button
+                    onClick={() => this.handleDelete(e, todo)}
+                    className="buttonstyle"
+                >
+                    {'✘'}
+                </button>
+                <button
+                    onClick={(e) => this.handleCheck(e, todo)}
+                    className="buttonstyle"
+                >
+                    {todo.finished ? '✔' : '-'}
+                </button>
+                <div className="panel">
+                    <p className={this.props.chosenTodoId == todo.todoId ? 'view' : 'noview'}>
+                        <form
+                            className="form"
+                        >
+                            {this.props.toggle}
+                            <input
+                                autoFocus="autofocus"
+                                className="input"
+                                maxLength="50"
+                                onChange={this.handleChange}
+                                placeholder="date"
+                                type="text"
+                                value={this.state.input}
+                            />
+                            <input
+                                autoFocus="autofocus"
+                                className="input"
+                                maxLength="50"
+                                onChange={this.handleChange}
+                                placeholder="categorie"
+                                type="text"
+                                value={this.state.input}
+                            />
+                        </form>
+                    </p>
+                </div>
+            </li>;
         });
+    }
 
-        const index = todos.findIndex(t => t.todoId === todoId);
-        let newTodo={};
-        Object.assign(newTodo,todo);
-        todos[index] = newTodo;
-        this.props.fetchTodos(todos);
-    };
-
-     handleDelete = (e,todoId) => {
-          if (confirm("Are you sure that you want to delete this todo?")) {
-              const todos = this.props.todos;
-              const todo = todos.find(todo => todo.todoId === todoId);
-
-              ajax('POST', `todos/delete?id=${todo.todoId}`, 5, 1000, () => {}, 
-              () => {
-                  alert('Could not delete todo from db with 5 tries at maximum 6 sec...');
-              });
-
-              const index = todos.findIndex(t => t.todoId === todoId);
-              todos.splice(index,1);
-              this.props.fetchTodos(todos);
-          }
-     };
-
-
-  render = () => (
-    <div className='list'>
-      <form
-        className="form"
-        onSubmit={this.handleSubmit}
-      >
-        {this.props.toggle}
-        <input
-          autoFocus="autofocus"
-          className="input"
-          maxLength="50"
-          onChange={this.handleChange}
-          placeholder="What are you going TODO ?"
-          type="text"
-          value={this.state.input}
-        />
-      </form>
-      <ol className='todos'>
-        {this.createTodos()}
-      </ol>
-    </div>
-  );
+    render = () => (
+        <div className='list'>
+            <form
+                className="form"
+                onSubmit={this.handleSubmit}
+            >
+                {this.props.toggle}
+                <input
+                    autoFocus="autofocus"
+                    className="input"
+                    maxLength="50"
+                    onChange={this.handleChange}
+                    placeholder="What are you going TODO ?"
+                    type="text"
+                    value={this.state.input}
+                />
+            </form>
+            <ol className='todos'>
+                {this.createTodos()}
+            </ol>
+        </div>
+    );
 }
 
 
 
 function mapStateToProps(state){
-  return {
-    todos: state.todos,
-    chosenTodoId: state.chosenTodoId,
-  };
+    return {
+        todos: state.todos,
+        chosenTodoId: state.chosenTodoId,
+    };
 }
 
-function matchDispatchToProps(dispatch){
-  return bindActionCreators(
-    {
-      fetchTodos: fetchTodos,
-      chooseTodo: chooseTodo
-    }, 
-    dispatch
-  );
-}
-
-
-
-export default connect(mapStateToProps, matchDispatchToProps)(List);
+export default connect(mapStateToProps, { fetchTodos, chooseTodo })(List);
