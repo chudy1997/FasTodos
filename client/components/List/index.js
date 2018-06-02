321f008e5bef3832f178e439ef70d16ca308e088
kiccek <kiccek@gmail.com>
2018-06-02 18:10:30 +0200

    first touches on list DND, dragging already working

--- client/components/List/index.js
+++ client/components/List/index.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import fetchTodos from './../../actions/fetchTodos';
 import chooseTodo from './../../actions/chooseTodo';
@@ -14,16 +15,66 @@ import ajax from './../../ajax';
 
 import './index.css';
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle, backgroundColor) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : backgroundColor,
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  //background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  //padding: grid,
+});
+
 class List extends Component {
+    constructor(props) {
+      super(props);
+      this.onDragEnd = this.onDragEnd.bind(this);
+    }
+
     state = {
       input: '',
       date: moment()
     };
 
+    onDragEnd(result) {
+      // dropped outside the list
+      if (!result.destination) {
+        return;
+      }
+
+      const todos = reorder(
+        this.props.todos,
+        result.source.index,
+        result.destination.index
+      );
+
+      this.setState({
+        todos,
+      });
+    }
+
     componentDidMount = () => {
       ajax('GET', 'todos', 5, 1000, todos => {
         this.props.fetchTodos(todos.sort((a, b) => a.deadline - b.deadline));
-      }, 
+      },
       () => {
         alert('Could not fetch todos...');
       });
@@ -48,14 +99,14 @@ class List extends Component {
       if (text.length > 0){
         const todos = this.props.todos;
         let categoryId = this.getCategoryId();
-        if (categoryId === 0) 
+        if (categoryId === 0)
         {categoryId = 1;}
 
         ajax('POST', `todos/new?text=${text}&categoryId=${categoryId}`, 5, 1000, res => {
           const newTodo = { todoId: res.insertId, text: text, categoryId: categoryId, finished: false };
           todos.unshift(newTodo);
           this.props.fetchTodos(todos);
-        }, 
+        },
         () => {
           alert('Could not add new todo...');
         });
@@ -70,7 +121,7 @@ class List extends Component {
       this.props.chooseTodo(this.props.chosenTodoId !== todo.todoId ? todo.todoId : -1);
     };
 
-    handleCheck = (e, todo) => {      
+    handleCheck = (e, todo) => {
       e.stopPropagation();
       const todos = this.props.todos;
       const oldValue = todo.finished;
@@ -78,7 +129,7 @@ class List extends Component {
 
       todo.finished = newValue;
       this.fetchChangedTodos(todos, todo);
-      ajax('POST', `todos/finish?id=${todo.todoId}&value=${newValue}`, 5, 1000, () => {}, 
+      ajax('POST', `todos/finish?id=${todo.todoId}&value=${newValue}`, 5, 1000, () => {},
         () => {
           alert('Could not check todo...');
           todo.finished = oldValue;
@@ -101,7 +152,7 @@ class List extends Component {
                ajax('POST', `todos/delete?id=${todo.todoId}`, 5, 1000, () => {
                  todos.splice(todos.findIndex(t => t.todoId === todo.todoId), 1);
                  this.props.fetchTodos(todos);
-               }, 
+               },
                () => {
                  alert('Could not delete todo...');
                });
@@ -135,61 +186,86 @@ class List extends Component {
     createTodos = (finished) => {
       const categoryId = this.getCategoryId();
       const todos = this.props.todos.filter(todo => categoryId === 0 || todo.categoryId === categoryId);
-      return todos.filter(todo => todo.finished == finished).map(todo => {
+      return todos.filter(todo => todo.finished == finished).map((todo,index) => {
         const backgroundColor =  this.props.colorMap[todo.categoryId%Object.keys(this.props.colorMap).length];
         const className = this.props.chosenTodoId == todo.todoId ? 'todo chosen-todo' : 'todo';
 
         return (
-          <li
-            className={className}
-            id={todo.todoId}
-            key={todo.todoId}
-            onClick={() => this.handleExpand(todo)}
-            style={{ backgroundColor: backgroundColor }}
-          >
-            <input 
-              checked={todo.finished ? 'checked' : ''} 
-              className='check' 
-              onClick={(e) => this.handleCheck(e, todo)}
-              type='checkbox' 
-            />
-            {todo.text}
-            <button
-              className="buttonstyle"
-              hidden={this.props.chosenTodoId === todo.todoId ? '' : 'hidden'}
-              onClick={(e) => this.handleDelete(e, todo)}
-            >
-              {'✘'}
-            </button>
-            <div className="panel">
-              <p className={this.props.chosenTodoId === todo.todoId ? 'view' : 'noview'}>
-                <form >
-                  <DatePicker
-                    className="Select"
-                    dateFormat="LLL"
-                    onChange={this.handleChangeDate}
-                    onClick={e => e.stopPropagation()}
-                    selected={this.state.date}
-                    showTimeSelect
-                    timeCaption="time"
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                  />
-                </form>
-                <select
-                  className="Select"
-                  onChange={(e) => this.selectCategory( e.target.value,todo)}
-                  onClick={e => e.stopPropagation()}
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
                 >
-                  {this.props.categories.map(category => (
-                    <option
-                      key={category.categoryId}
-                      value={category.categoryId}
-                    >{ category.categoryName }</option>))}
-                </select>
-              </p>
-            </div>
-          </li>);
+                    <Draggable key={todo.todoId} draggableId={todo.todoId} index={index} >
+                      {(provided, snapshot) => (
+                        <li
+                          className={className}
+                          id={todo.todoId}
+                          key={todo.todoId}
+                          onClick={() => this.handleExpand(todo)}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style,
+                            backgroundColor
+                          )}
+                        >
+                          <input
+                            checked={todo.finished ? 'checked' : ''}
+                            className='check'
+                            onClick={(e) => this.handleCheck(e, todo)}
+                            type='checkbox'
+                          />
+                          {todo.text}
+                          <button
+                            className="buttonstyle"
+                            hidden={this.props.chosenTodoId === todo.todoId ? '' : 'hidden'}
+                            onClick={(e) => this.handleDelete(e, todo)}
+                          >
+                            {'✘'}
+                          </button>
+                          <div className="panel">
+                            <p className={this.props.chosenTodoId === todo.todoId ? 'view' : 'noview'}>
+                              <form >
+                                <DatePicker
+                                  className="Select"
+                                  dateFormat="LLL"
+                                  onChange={this.handleChangeDate}
+                                  onClick={e => e.stopPropagation()}
+                                  selected={this.state.date}
+                                  showTimeSelect
+                                  timeCaption="time"
+                                  timeFormat="HH:mm"
+                                  timeIntervals={15}
+                                />
+                              </form>
+                              <select
+                                className="Select"
+                                onChange={(e) => this.selectCategory( e.target.value,todo)}
+                                onClick={e => e.stopPropagation()}
+                              >
+                                {this.props.categories.map(category => (
+                                  <option
+                                    key={category.categoryId}
+                                    value={category.categoryId}
+                                  >{ category.categoryName }</option>))}
+                              </select>
+                            </p>
+                          </div>
+                        </li>
+
+                      )}
+                    </Draggable>
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+          );
       });
     };
 
