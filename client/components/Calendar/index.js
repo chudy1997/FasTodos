79b12d4c0936337142234745a8f95265d83be452
Adam Dyszy <electro.ubro@gmail.com>
2018-06-10 19:49:31 +0200

    fixed duplicate WeekCalendar

--- client/components/Calendar/index.js
+++ client/components/Calendar/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import WeekCalendar from 'react-week-calendar';
 import 'react-week-calendar/dist/style.css';
 
 import { DragDropContext } from 'react-dnd';
